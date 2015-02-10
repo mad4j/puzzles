5a6ef0e28cae813e8ba49fe0715c3059c5dc935e
Daniele Olmisani <daniele.olmisani@gmail.com>
2015-02-10 21:46:16 +0100

    few optimizations

--- src/dolmisani/puzzles/Queens5by5.java
+++ src/dolmisani/puzzles/Queens5by5.java
@@ -10,7 +10,7 @@ public class Queens5by5 {
 
 	public static void generate(int[] s, int position, int nextInt, int k, int N) {
 		if (position == k) {
-			if (computeAttack(s, 5) == 3) {
+			if (computeAttack(s, 5) == 5) {
 				showCombination(s);
 			}
 			return;
@@ -22,32 +22,30 @@ public class Queens5by5 {
 	}
 	
 	
+	public static boolean attack(int c, int q, int size) {
+		return ((q%size == c%size) || 
+				(q/size == c/size) || 
+				((q%size+q/size) == (c%size+c/size)) || 
+				((q%size-q/size) == (c%size-c/size)));
+	}
+	
 	public static int computeAttack(int[] queens, int size) {
-		
+
 		int result = 0;
-		int[][] board = new int[size][size];
-		
-		for (int q=0; q<queens.length; q++) {
+		int[] board = new int[size*size];
+				
+		for (int q: queens) {
+			for (int c=0; c<board.length; c++) {
 			
-			int x = queens[q] / 5;
-			int y = queens[q] % 5;
-			
-			for (int i=0; i<size; i++) {
-				for (int j=0; j<size; j++) {
-					if ((x==i) || (y==j)|| ((x+y)==(i+j)) || ((x-y)==(i-j))) {
-						board[i][j] = 1;
-					}
+				if  (attack(c, q, size)) {					
+					board[c] = 1;
 				}
 			}
 		}
 		
-		
-		for (int i=0; i<size; i++) {
-			for (int j=0; j<size; j++) {
-				
-				if (board[i][j] == 0) {
-					result++;
-				}
+		for (int c=0; c<board.length; c++) {
+			if (board[c] == 0) {
+				result++;
 			}
 		}
 		
@@ -56,7 +54,7 @@ public class Queens5by5 {
 
 	public static void main(String[] args) {
 		int N = 25;
-		int k = 5;
+		int k = 3;
 
 		int[] s = new int[k];
 		generate(s, 0, 0, k, N);
